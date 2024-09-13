98059ea4cc55f22a7abfb7e037de307e64956d9d
Kalpana_Perera <118042669+KalpanaPerera@users.noreply.github.com>
2024-09-13 23:59:19 +0530

    update 2

--- src/BridgePattern.java
+++ src/BridgePattern.java
@@ -9,4 +9,26 @@ abstract class TV
     abstract void off();
 }
 
+class Sony extends TV
+{
+    Remote remoteType;
+    Sony(Remote r) 
+    {
+        super(r);
+        this.remoteType = r;
+    }
+  
+    public void on()
+    {
+        System.out.print("Sony TV ON: ");
+        remoteType.on();
+    }
+    
+    public void off()
+    {
+        System.out.print("Sony TV OFF: ");
+        remoteType.off();
+    }
+}
+  
 }
